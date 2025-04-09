73b0bfe01d10017275217e02524601814af47cb0
Aryaman Gupta <108190611+AryamanGupta001@users.noreply.github.com>
2025-04-09 16:28:46 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,6 @@
 const CONTACT_INFO = {
-  email: "ambienceofficial@gmail.com",
-  phone: "+91-9876543210"
+  email1: "Shiv@ambienceautomations.com",
+  email2: "Bhupinder@ambienceautomations.com",
+  phone: "+91-9592026120",
+  address: "Plot No. 326, Industrial Area, Phase 9, Mohali, Punjab 160062"
 };
